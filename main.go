9fc51cb4c654a4f9856e34ec9a9e8ac9d5de1c37
Jake Mackin <canesfanjake1@gmail.com>
2026-04-28 10:23:16 -0400

    added attributes to the errors, new linkoerr module

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"syscall"
 	"time"
 
+	"boot.dev/linko/internal/linkoerr"
 	"boot.dev/linko/internal/store"
 	pkgerr "github.com/pkg/errors"
 )
@@ -23,15 +24,19 @@ func replaceAttr(groups []string, a slog.Attr) slog.Attr {
 		if !ok {
 			return a
 		}
+		errAttribs := linkoerr.Attrs(err)
+		base := []slog.Attr{
+			slog.String("message", err.Error()),
+		}
+		combined := append(base, errAttribs...)
 		if stackErr, ok := errors.AsType[stackTracer](err); ok {
-			return slog.GroupAttrs("error", slog.Attr{
-				Key:   "message",
-				Value: slog.StringValue(stackErr.Error()),
-			}, slog.Attr{
+			stackTraceAttr := slog.Attr{
 				Key:   "stack_trace",
 				Value: slog.StringValue(fmt.Sprintf("%+v", stackErr.StackTrace())),
-			})
+			}
+			combined = append(combined, stackTraceAttr)
 		}
+		return slog.GroupAttrs("error", combined...)
 	}
 	return a
 }
