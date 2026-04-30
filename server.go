a0f3a8939a87a422b46dd2eeae246060770c3a0b
Jake Mackin <canesfanjake1@gmail.com>
2026-04-30 10:22:22 -0400

    changed HTTP error logging to only include generic messages for 401, 403, and 500 errors. other errors will still have the full error message

--- server.go
+++ server.go
@@ -105,7 +105,11 @@ func httpError(ctx context.Context, w http.ResponseWriter, status int, err error
 	if logCtx, ok := ctx.Value(logContextKey).(*LogContext); ok {
 		logCtx.Error = err
 	}
-	http.Error(w, err.Error(), status)
+	if status == 401 || status == 403 || status == 500 {
+		http.Error(w, http.StatusText(status), status)
+	} else {
+		http.Error(w, err.Error(), status)
+	}
 }
 
 func requestID() func(http.Handler) http.Handler {
